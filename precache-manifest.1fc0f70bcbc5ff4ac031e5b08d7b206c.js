420ee8eed46c2f7d6d023d13b86c98e5e01ce55b
Luis Arturo Noria <lnoria86@outlook.com>
2020-07-07 15:40:35 -0500

    Updates

---
+++ precache-manifest.1fc0f70bcbc5ff4ac031e5b08d7b206c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcee34036cdf41b632f42124905eaa52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7b2f3f348ac0ec408db9",
+    "url": "/monsters-rolodex/static/css/main.cbe3c42f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2f3f348ac0ec408db9",
+    "url": "/monsters-rolodex/static/js/main.293aad37.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
